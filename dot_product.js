81384c3b1f72d1e47c02939603a2b7cc61a845ea
Maria Kesa <maria.kesa@gmail.com>
2024-12-29 23:56:00 +0200

    fixed angle

--- dot_product.js
+++ dot_product.js
@@ -22,14 +22,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 3. Draw a Straight Line with an Arrowhead
     svg.append("line")
-      .attr("x1", 10)
-      .attr("y1", 10)
-      .attr("x2", 90)
-      .attr("y2", 90)
+      .attr("x1", 25)
+      .attr("y1", 100)
+      .attr("x2", 50)
+      .attr("y2", 50)
       .attr("stroke", "black")
       .attr("stroke-width", 2)
       .attr("marker-end", "url(#arrow)");
 
+    svg.append("line")
+      .attr("x1", 25)
+      .attr("y1", 100)
+      .attr("x2", 125)
+      .attr("y2", 75)
+      .attr("stroke", "black")
+      .attr("stroke-width", 2)
+      .attr("marker-end", "url(#arrow)");
+
+    function calculateAngle(x1, y1, x2, y2) {
+        return Math.atan2(y2 - y1, x2 - x1) + Math.PI / 2;
+    }
+    const angle1 = calculateAngle(25, 100, 50, 50);
+    const angle2 = calculateAngle(25, 100, 125, 75);
+
+    console.log(angle1, angle2);
+
+    var arcGenerator= d3.arc();
+
+    const angleArc = arcGenerator({
+        innerRadius: 0,
+        outerRadius: 10,
+        startAngle: angle1,
+        endAngle: angle2
+    });
+
+    svg.append("path")
+      .attr("d", angleArc)
+      .attr("fill", "red")
+      .attr("transform", "translate(25, 100)");
 
     
 });
